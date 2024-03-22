ddaf66d28ac58a51180c6935fbe9048d11939700
Mario Eduardo <eduardoat44@gmail.com>
2024-03-22 10:37:26 -0600

    Configuracion inicial

---
+++ models/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    // MIDDLEWARES
+    this.middlewares();
+    // ROUTES
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en el puerto ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
